5c1efff03648c2b049af9dce51691e07e7029d3c
hex7c0 <carniellifrancesco@gmail.com>
2014-08-11 16:50:24 +0200

    index

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * @file REPLACE main
+ * @module REPLACE
+ * @package REPLACE
+ * @subpackage main
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @copyright hex7c0 2014
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+
+/*
+ * functions
+ */
