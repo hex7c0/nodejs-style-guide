abb28a56d0b60b57ac6876983d9cf0b86ffd157b
hex7c0 <carniellifrancesco@gmail.com>
2015-02-05 10:19:15 +0100

    remove try...catch block

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 /**
  * @file REPLACE main
  * @module REPLACE
- * @package REPLACE
  * @subpackage main
  * @version 0.0.1
  * @author hex7c0 <hex7c0@gmail.com>
- * @copyright hex7c0 2014
+ * @copyright hex7c0 2015
  * @license GPLv3
  */
 
@@ -14,12 +13,6 @@
  * initialize module
  */
 // import
-try {
-
-} catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
-}
 
 /*
  * functions
